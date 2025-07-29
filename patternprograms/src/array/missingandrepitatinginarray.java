dcb28403360a0ebf6bd3f9ac016678660d59891b
satya <satyanarayanasatya2000@gmail.com>
2025-07-29 21:57:55 +0530

    h

--- patternprograms/src/array/missingandrepitatinginarray.java
+++ patternprograms/src/array/missingandrepitatinginarray.java
@@ -7,7 +7,8 @@ public class missingandrepitatinginarray {
 	public static void main(String[] args) {
 		int count=1;
 		int max=0;
-		int a[]= {3, 1, 3,4,4,4};
+		int repetiveelement = 0;
+		int a[]= {3, 1, 3,4,5,5,5};
 		for(int i=0;i<a.length-1;i++) {
 			if(a[i]>=a[i+1]) {
 				int p=a[i+1];
@@ -17,10 +18,22 @@ public class missingandrepitatinginarray {
 			
 		}
 		System.out.println(Arrays.toString(a));
-		for(int i=0;i<a.length;i++) {
+		for(int i=0;i<a.length-1;i++) {
+			if((a[i]+1!=a[i+1])&& a[i]+1<a[i+1]) {
+				System.out.println(a[i]+1);
+				
+			}else if(a[i]==a[i+1]) {
+				count++;
+				if(count>=max) {
+					repetiveelement=a[i];
+				}
+				max=count;
+				count=1;
+			}
 			
 			
 		}
+		System.out.println(repetiveelement);
 
 	}
 
